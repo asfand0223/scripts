202e33f24265ffc381d07ec8d3d6948d71ce074e
asfand0223 <asfand0223@gmail.com>
2025-04-20 23:26:21 +0100

    script to setup react project in tmux

--- bash/start-nexus.sh
+++ bash/start-nexus.sh
@@ -1,18 +1,71 @@
 #!/usr/bin/env bash
 
-tmux new-session -ds nexus
-tmux new-window -t nexus -n client
-tmux new-window -t nexus -n server
-tmux kill-window -t nexus:0
-tmux split-window -ht nexus:client
-tmux split-window -ht nexus:server
+PROJECT_NAME=$1
+WINDOW_NAME=$2
+PROJECT_DIR=$HOME/git/$1
+ 
+select_project() {
+    find "$PROJECT_DIR" -maxdepth 2 -type d | fzf --prompt="Please select a react project:"
+}
 
-react_proj=$HOME/git/nexus/Nexus.Client
-dotnet_sol=$HOME/git/nexus/Nexus.Server
-dotnet_proj=$HOME/git/nexus/Nexus.Server/Nexus.Server
+setup_tmux_session() {
+    local session_name=$1
 
-tmux send-keys -t nexus:client.0 "cd $react_proj && nvim" C-m
-tmux send-keys -t nexus:client.1 "cd $react_proj && yarn run dev" C-m
-tmux send-keys -t nexus:server.0 "cd $dotnet_sol && nvim" C-m
-tmux send-keys -t nexus:server.1 "cd $dotnet_proj && dotnet build -c Debug && dotnet run" C-m
-tmux attach-session -t nexus
+    if [[ -z $session_name ]]; then
+	echo "Failed to setup tmux session - missing args"
+	return
+    fi
+
+    tmux new-session -ds "$session_name"
+}
+
+setup_window() {
+    local session_name=$1
+    local window_name=$2
+    local project=$3
+
+    if [[ -z $session_name || -z $window_name || -z $project ]]; then
+	echo "Failed to setup window - missing args"
+	return
+    fi
+
+    tmux new-window -t "$session_name" -n "$window_name"
+    tmux kill-window -t "$session_name":0
+    tmux split-window -ht "$session_name":"$window_name"
+    tmux send-keys -t "$session_name:$window_name.0" "cd \"$project\" && nvim" C-m
+    tmux send-keys -t "$session_name:$window_name.1" "cd \"$project\" && yarn run dev" C-m
+}
+
+main() {
+    if [[ -z $PROJECT_NAME ]]; then
+	echo "Please provide a session name"
+	return
+    fi
+    if [[ -z $WINDOW_NAME ]]; then
+	echo "Please provide a window name"
+	return
+    fi
+
+    local project=$(select_project)
+    if [[ -z $project ]]; then
+	echo "No project selected"
+	return
+    fi
+
+    if tmux has-session -t "$PROJECT_NAME" 2>/dev/null; then
+	echo "Session already exists. Please close existing one."
+	return
+    fi
+
+    if [[ ! -f $project/package.json ]]; then
+	echo "Invalid react project"
+	return
+    fi
+
+    setup_tmux_session $PROJECT_NAME
+    setup_window $PROJECT_NAME $WINDOW_NAME $project
+    tmux attach-session -t "$PROJECT_NAME"
+}
+
+main
+
