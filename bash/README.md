c82671ae22fb1a49b38a90a334fee1af98377000
asfand0223 <asfand0223@gmail.com>
2025-04-21 12:14:53 +0100

    Script that creates a tmux session with a split window containing the csharp sln in neovim and the csproj running in the other plane

--- bash/README.md
+++ bash/README.md
@@ -1,2 +1,7 @@
-React.sh
+react.sh
+
+- Script that sets up a tmux session with a horizontally split window where the left pane is the project open in neovim and the right pane is the project running
+
+csharp.sh
+
 - Script that sets up a tmux session with a horizontally split window where the left pane is the project open in neovim and the right pane is the project running
---
+++ bash/csharp.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=$1
+WINDOW_NAME=$2
+PROJECT_DIR=$HOME/git/$1
+ 
+select_project() {
+    find "$PROJECT_DIR" -maxdepth 3 -type d | fzf --prompt="Please select a react project:"
+}
+
+setup_tmux_session() {
+    local session_name=$1
+
+    if [[ -z $session_name ]]; then
+	echo "Failed to setup tmux session - missing args"
+	return
+    fi
+
+    tmux new-session -ds "$session_name"
+}
+
+setup_window() {
+    local session_name=$1
+    local window_name=$2
+    local project=$3
+
+    if [[ -z $session_name || -z $window_name || -z $project ]]; then
+	echo "Failed to setup window - missing args"
+	return
+    fi
+
+    tmux new-window -t "$session_name" -n "$window_name"
+    tmux kill-window -t "$session_name":0
+    tmux split-window -ht "$session_name":"$window_name"
+    tmux send-keys -t "$session_name:$window_name.0" "cd \"$project\"/.. && nvim" C-m
+    tmux send-keys -t "$session_name:$window_name.1" "cd \"$project\" && dotnet build -c Debug && dotnet run" C-m
+}
+
+main() {
+    if [[ -z $PROJECT_NAME ]]; then
+	echo "Please provide a session name"
+	return
+    fi
+    if [[ -z $WINDOW_NAME ]]; then
+	echo "Please provide a window name"
+	return
+    fi
+
+    local project=$(select_project)
+    if [[ -z $project ]]; then
+	echo "No project selected"
+	return
+    fi
+
+    if tmux has-session -t "$PROJECT_NAME" 2>/dev/null; then
+	echo "Session already exists. Please close existing one."
+	return
+    fi
+
+    if ! find "$project" -maxdepth 1 -type f | grep -q "\.csproj$"; then
+	echo "Invalid csharp project"
+	return
+    fi
+
+    setup_tmux_session $PROJECT_NAME
+    setup_window $PROJECT_NAME $WINDOW_NAME $project
+    tmux attach-session -t "$PROJECT_NAME"
+}
+
+main
+
